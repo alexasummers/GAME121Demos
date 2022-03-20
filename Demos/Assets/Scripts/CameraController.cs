b3da3c4a5db03fd149d21c96940bec8eab736def
Alexa Summers <alexas0202@gmail.com>
2022-03-19 23:54:16 -0500

    Add FPS Demo

--- Demos/Assets/Scripts/CameraController.cs
+++ Demos/Assets/Scripts/CameraController.cs
@@ -14,16 +14,13 @@ public class CameraController : MonoBehaviour
    {
        _fpsCamera = Camera.main; //Accessing our object camera
        parent = transform.parent; //the parent of our object is the object we want to rotate
-       Cursor.lockState = CursorLockMode.Locked; //locks mouse to the center of the screen
    }
 
    private void Update()
    {
     float horizontalRotation = Input.GetAxis("Mouse X") * _mouseMovement * Time.deltaTime; //horizontal rotation calculation
-    float verticalRotation = Input.GetAxis("Mouse Y") * _mouseMovement * Time.deltaTime; //vertical rotation calculation
      
     parent.Rotate(0, horizontalRotation, 0); //rotate parent around the vertical axis-- horizontal movement
-    _fpsCamera.transform.Rotate(-verticalRotation, 0, 0); //rotate camera around the horizontal axis-- vertical movement
 
     }
 }
